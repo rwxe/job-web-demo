8295c83df66ba01070069f696be17baac6b210cf
yizdu <jzunk@protonmail.com>
2022-04-30 00:46:53 +0800

    fix: 脚本增加关闭sql_mode的语句
    mysql默认开启ONLY_FULL_GROUP_BY的sql_mode，关闭才能使用

--- init_env_var.sh
+++ init_env_var.sh
@@ -3,3 +3,6 @@ export FLASK_APP=manage.py
 # 初始项目根目录到环境变量中
 # 确保部分相对引用可用
 export  PYTHONPATH=${PYTHONPATH}:$PWD
+# 关闭mysql默认的ONLY_FULL_GROUP_BY sql_mode
+#mysql -h 主机地址 -u用户名 -p密码 -e "set @@GLOBAL.sql_mode='';"""
+mysql -h localhost -uroot -proot -e "set @@GLOBAL.sql_mode='';"""
