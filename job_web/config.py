b023b2b873c3de71168e98c5cadc8adc500617e6
yizdu <jzunk@protonmail.com>
2022-04-29 22:19:27 +0800

    更新数据库配置文件
    理论上，配置文件不应该更新到git中，但是项目的MySQL用户是专用的，
    安全问题不是什么大问题，而且只能本地访问

--- job_web/config.py
+++ job_web/config.py
@@ -3,7 +3,7 @@ from flask_uploads import IMAGES
 
 
 class BaseConfig(object):
-    SECRET_KEY = 'makesure to set a very secret key'
+    SECRET_KEY = '123456'
     JOB_INDEX_PER_PAGE = 18
     COMPANY_INDEX_PER_PAGE = 20
     COMPANY_DETAIL_PER_PAGE = 10
@@ -12,9 +12,10 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:123456@localhost:3306/job_web?charset=utf8'
+    # mysql://username:password@server_name/db_name
+    # 项目的mysql用户为jw，密码jw
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://jw:jw@localhost:3306/job_web?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     UPLOADED_SIZE = 300 * 1024
     UPLOADED_RESUME_ALLOW = IMAGES
     UPLOADED_RESUME_DEST = os.path.join(os.getcwd(), 'static', 'resume')
